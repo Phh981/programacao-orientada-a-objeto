41b030c618c38794075a6da423cd731334cd119d
Henrique <paulorocha@sempreceub.com>
2025-12-01 15:47:16 -0300

    corrigindo o erro pelo código real do Sistema de Cambio

--- SistemaCambio/Program.cs
+++ SistemaCambio/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using SistemaCambio;
+
+HttpClient cliente = new HttpClient();
+
+Console.WriteLine("  SISTEMA DE CÂMBIO ");
+Console.WriteLine("Consultando cotação do Dólar...");
+
+try
+{
+    string url = "https://economia.awesomeapi.com.br/last/USD-BRL";
+
+    HttpResponseMessage resposta = await cliente.GetAsync(url);
+
+    resposta.EnsureSuccessStatusCode();
+
+    string jsonResposta = await resposta.Content.ReadAsStringAsync();
+    var dados = JsonConvert.DeserializeObject<dynamic>(jsonResposta);
+
+    string jsonDolar = dados["USDBRl"].ToString();
+    Moeda? dolar = JsonConvert.DeserializeObject<Moeda>(jsonDolar);
+
+    if (dolar != null) 
+    {
+    dolar.ExibirDetalhes();
+    }
+}
+catch (Exception erro)
+{
+    Console.WriteLine($"Deu ruim: {erro.Message}");
+}
